89a79314aa6879ee6ea28ca73ec452f75ae0c902
Varun Kumar <Varun251005@users.noreply.github.com>
2026-09-23 21:26:13 +0530

    add built-in command execution for cd and pwd

---
+++ src/builtins.rs
@@ -0,0 +1,31 @@
+pub enum BuiltinStatus {
+    Handled,
+    Exit,
+    NotHandled,
+}
+
+pub fn execute(program: &str, arguments: &[&str]) -> BuiltinStatus {
+    match program {
+        "exit" => BuiltinStatus::Exit,
+        "cd" => {
+            if arguments.is_empty() {
+                eprintln!("jodsh: cd: missing directory");
+            } else {
+                let target_dir = arguments[0];
+                if let Err(e) = std::env::set_current_dir(target_dir) {
+                    eprintln!("jodsh: cd: {}", e);
+                }
+            }
+            BuiltinStatus::Handled
+        }
+        "pwd" => {
+            match std::env::current_dir() {
+                Ok(dir) => println!("{}", dir.display()),
+                Err(e) => eprintln!("jodsh: pwd: {}", e),
+            }
+            BuiltinStatus::Handled
+        }
+        _ => BuiltinStatus::NotHandled,
+    }
+}
+
